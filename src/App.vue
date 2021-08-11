4c7b533a67f6d2b8110244688fb7374df160f231
Allexx <allexxberesnev@yandex.ru>
2021-08-11 11:39:55 +0700

    First

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <h1>Todo application</h1>
+    <hr>       
+    <router-view />
+
 </template>
 
 <style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <h2>    
+            Home page
+        </h2>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non est quod fugiat sint in modi aliquid quia doloremque voluptates voluptatibus.
+        </p>
+        <router-link to="/todos">Todo List</router-link>
+    </div>    
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
